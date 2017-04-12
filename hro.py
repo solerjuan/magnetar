01364e8d269d847c76dc1832664a929af5fc910c
Juan Soler <juandiegosolerp@gmail.com>
2017-04-12 19:19:14 +0200

    Changes unified in hro.py

--- hro.py
+++ hro.py
@@ -1,80 +1,71 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.convolution import convolve, convolve_fft
+from astropy.convolution import Gaussian2DKernel
+
+def roangles(Imap, Qmap, Umap):
+
+	psi=0.5*np.arctan2(-Umap,Qmap)	
+	ex=np.sin(psi)
+    	ey=np.cos(psi)
+
+    	grad=np.gradient(Imap, edge_order=2)
+	#kernel=Gaussian2DKernel(3)
+	#grad=np.gradient(convolve_fft(Imap, kernel))	
+	phi=np.arctan2(grad[0]*ex-grad[1]*ey, grad[0]*ey+grad[1]*ex)		
+
+	bad=np.logical_or(grad[0]*grad[0]+grad[1]*grad[1]==0., Qmap*Qmap+Umap*Umap==0.).nonzero()	
+	phi[bad]=np.sqrt(-1)
+
+    	return np.abs(phi)
 
 
-def primes(imax):
-    """
-    Returns prime numbers up to imax.
+def hro(Imap, Qmap, Umap, steps=10, hsize=20, minI=0.):
 
-    Parameters
-    ----------
-    imax: int
-        The number of primes to return. This should be less or equal to 10000.
+	sz=np.shape(Imap)
+	phi=roangles(Imap, Qmap, Umap)
 
-    Returns
-    -------
-    result: list
-        The list of prime numbers.
-    """
+	hist, bin_edges = np.histogram(Imap[(Imap > minI).nonzero()], bins=100*sz[0])
+	bin_centre=0.5*(bin_edges[0:np.size(bin_edges)-1]+bin_edges[1:np.size(bin_edges)])
 
-    p = list(range(10000))
-    result = []
-    k = 0
-    n = 2
+	chist=np.cumsum(hist)
+	pitch=np.max(chist)/float(steps)
+	
+	hsteps=pitch*np.arange(0,steps+1,1)	
+	Isteps=np.zeros(steps+1)
 
-    if imax > 10000:
-        raise ValueError("imax should be <= 10000")
+	for i in range(0, np.size(Isteps)-1):
+		good=np.logical_and(chist>hsteps[i],chist<hsteps[i+1]).nonzero()
+		Isteps[i]=np.min(bin_centre[good])	
+	Isteps[np.size(Isteps)-1]=np.max(Imap)
+	print(Isteps)
 
-    while len(result) < imax:
-        i = 0
-        while i < k and n % p[i] != 0:
-            i = i + 1
-        if i == k:
-            p[k] = n
-            k = k + 1
-            result.append(n)
-            if k > 10000:
-                break
-        n = n + 1
+	hros=np.zeros([steps,hsize])	
+	Smap=0.*Imap
 
-    return result
+	for i in range(0, np.size(Isteps)-1):
+		good=np.logical_and(Imap>Isteps[i],Imap<Isteps[i+1]).nonzero()
+		print(np.size(good))
+		hist, bin_edges = np.histogram((180/np.pi)*phi[good], bins=hsize, range=(0.,180.))	
+		bin_centre=0.5*(bin_edges[0:np.size(bin_edges)-1]+bin_edges[1:np.size(bin_edges)])
+		hros[i,:]=hist
+		Smap[good]=i
+		plt.plot(bin_centre, hist)
+	plt.show()	
 
+	import pdb; pdb.set_trace()
 
-def do_primes(n, usecython=False):
-    if usecython:
-        from .example_c import primes as cprimes
-        print('Using cython-based primes')
-        return cprimes(n)
-    else:
-        print('Using pure python primes')
-        return primes(n)
+        return hros, Isteps
 
 
 def main(args=None):
 
-    from astropy.utils.compat import argparse
-    from time import time
+    	if res.prnt:
+		print('Primes: {0}'.format(primes))
 
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-c', '--use-cython', dest='cy', action='store_true',
-                        help='Use the Cython-based Prime number generator.')
-    parser.add_argument('-t', '--timing', dest='time', action='store_true',
-                        help='Time the Fibonacci generator.')
-    parser.add_argument('-p', '--print', dest='prnt', action='store_true',
-                        help='Print all of the Prime numbers.')
-    parser.add_argument('n', metavar='N', type=int,
-                        help='Get Prime numbers up to this number.')
+Imap=fits.open('data/Taurusfwhm5_logNHmap.fits')
+Qmap=fits.open('data/Taurusfwhm10_Qmap.fits')
+Umap=fits.open('data/Taurusfwhm10_Umap.fits')
+hro(Imap[0].data, Qmap[0].data, Umap[0].data, minI=21.0)
 
-    res = parser.parse_args(args)
-
-    pre = time()
-    primes = do_primes(res.n, res.cy)
-    post = time()
-
-    print('Found {0} prime numbers'.format(len(primes)))
-    print('Largest prime: {0}'.format(primes[-1]))
-
-    if res.time:
-        print('Running time: {0} s'.format(post - pre))
-
-    if res.prnt:
-        print('Primes: {0}'.format(primes))
