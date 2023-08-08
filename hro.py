Bug fixed in S2 calculation
Fixed index bug in the structure function

hro.py
@@ -242,8 +242,11 @@ def hroLITE(Imap, Qmap, Umap, steps=10, hsize=15, minI=None, mask=None, ksz=1, s
    for i in range(0,steps):
       good=np.logical_and(chist>hsteps[i], chist<=hsteps[i+1]).nonzero()
       Isteps[i]=np.min(bin_centre[good])	
-
    Isteps[np.size(Isteps)-1]=np.nanmax(stepmap)
+
+   for i in range(0,steps+1):
+      diff=np.abs(i*pitch-chist)
+      Isteps[i]=np.mean(bin_centre[(diff==np.nanmin(diff)).nonzero()])
 
    # Preparing output of the HRO
    hros=np.zeros([steps,hsize])

structurefunction.py
@@ -137,13 +137,12 @@ def structurefunction(Qmap, Umap, lag=4.0, s_lag=1.0, mask=None, header=None):
    sz=np.shape(Qmap)
 
    if header is None:
-      posx, posy=np.meshgrid(np.arange(0,sz[0]), np.arange(0,sz[0])) 
+      posx, posy = np.meshgrid(np.arange(0,sz[0]), np.arange(0,sz[1])) 
    else:
       ra=header['CRVAL1']+header['CDELT1']*(np.arange(header['NAXIS1'])-header['CRPIX1'])
       dec=header['CRVAL2']+header['CDELT2']*(np.arange(header['NAXIS2'])-header['CRPIX2'])
       posx, posy=np.meshgrid(ra, dec)
 
-   
    x1, x2 =np.meshgrid(posx.ravel(), posx.ravel()) 
    deltax=(x1-x2)
    x1=None; x2=None
@@ -153,7 +152,7 @@ def structurefunction(Qmap, Umap, lag=4.0, s_lag=1.0, mask=None, header=None):
    print('Calculation of distances')
    dist=np.sqrt(deltax**2+deltay**2)
    deltax=None; deltay=None;
-   
+
    good1, good2 = np.logical_and(dist >= lag-s_lag, dist < lag+s_lag).nonzero()
 
    maskvec=mask.ravel() 