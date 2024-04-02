4c7c3104cbd13996f3e9d942e1a0f70c9405c992
Juan Soler <10508683+solerjuan@users.noreply.github.com>
2024-04-02 11:50:59 +0200

    hro3D updated
    Now includes: error in relative orientation parameter, statistical weights, and weighted mean cosine.

--- hro3D.py
+++ hro3D.py
@@ -10,24 +10,24 @@ from matplotlib.pyplot import cm
 
 from scipy import ndimage
 
+from tqdm import tqdm
 
+# ========================================================================================================================
 def roangles3D(dens, Bx, By, Bz, mode='nearest', pxksz=3):
     """
-    Calculates the relative orientation angles between the density structures 
-        and the magnetic field.
-    
+    Calculates the cosine of the relative orientation angles between the density gradient and the magnetic field in 3D. 
     
         Parameters
         ----------
         dens : numpy.ndarray. 
-             density field.
-        Bx, y, z : float or numpy.ndarray
-            	 magnetic field strength in each direction.
+               Density field.
+
+        Bx, By, Bz : float or numpy.ndarray
+            	     Components of the 3D magnetic field vector.
     
         Returns
         -------
-        numpy.ndarray of the relative angles between the density structure and 
-    	    and magnetic field structure.
+        cosphi: numpy.ndarray containing the cosine of the relative orientation angles between the density gradient and the magnetic field.
     
         Notes
         -----
@@ -41,16 +41,10 @@ def roangles3D(dens, Bx, By, Bz, mode='nearest', pxksz=3):
         --------
     	...
     """
-    # Calculates the relative orientation angles between the density structures and the magnetic field.
-    # INPUTS
-    # dens - regular cube with the values of density 
-    # Bx   -
-    # By   -
-    # Bz   -
-    #
-    # OUTPUTS
-    #
-    #
+
+    assert np.shape(dens) == np.shape(Bx), "Dimensions of dens and Bx must match"
+    assert np.shape(dens) == np.shape(By), "Dimensions of dens and By must match"  
+    assert np.shape(dens) == np.shape(Bz), "Dimensions of dens and Bz must match"  
     
     #gx=grad[1]; gy=grad[0]; gz=grad[2];
     gx=ndimage.filters.gaussian_filter(dens, [pxksz, pxksz, pxksz], order=[0,0,1], mode=mode)
@@ -66,185 +60,177 @@ def roangles3D(dens, Bx, By, Bz, mode='nearest', pxksz=3):
     cross=np.sqrt((gy*Bz-gz*By)**2+(gx*Bz-gz*Bx)**2+(gx*By-gy*Bx)**2)
     dot  =gx*Bx+gy*By+gz*Bz	
     
-    # The cosine of the angle between the iso-density and B is the sine of the angle between
-    # the density gradient and B.	
     cosphi=dot/(normgrad*normb)   
- 
     cosphi[(normgrad == 0.).nonzero()]=np.nan
     cosphi[(normb    == 0.).nonzero()]=np.nan
     
     return cosphi
 
 
-def equibins(dens, steps=10, mind=0.):
-    """
-    ...
+# ============================================================================================================
+def equibins(dens, steps=10, mind=None):
+   # Compute bins with equal number of voxels 
+   #
+   # INPUTS
+   # dens       - input cube 
+   # steps      - number of bins
+   # mind       - minimun values of the input array  
+   #
+   # OUTPUTS
+   # bincentres - 
+
+   if (mind is None):
+      mind=np.nanmin(dens)
+ 
+   sz=np.size(dens)
+   hist, bin_edges = np.histogram(dens[(dens > mind).nonzero()], bins=sz)
+   bin_centre     =0.5*(bin_edges[0:np.size(bin_edges)-1]+bin_edges[1:np.size(bin_edges)])
     
-    
-        Parameters
-        ----------
-        dens : numpy.ndarray. 
-             density field.
-        steps: int
-             ...
-        nubd : float
-             ...
-    
-        Returns
-        -------
-        ...
-    
-        Notes
-        -----
-    	...
-    
-        References
-        ----------
-        .. [1] Soler et al 2013 ...
-    
-        Examples
-        --------
-    	...
-    """
-    # Calculates the relative orientation angles between the density structures and the magnetic field.
-    # INPUTS
-        # dens - regular cube with the values of density 
-    # steps -
-    # mind - 
-    
-    sz=np.shape(dens)
-    hist, bin_edges = np.histogram(dens[(dens > mind).nonzero()], bins=10*sz[0]*sz[1])
-    bin_centre     =0.5*(bin_edges[0:np.size(bin_edges)-1]+bin_edges[1:np.size(bin_edges)])
-    
-    chist=np.cumsum(hist)
-    pitch=np.max(chist)/float(steps)
-    
-    hsteps=pitch*np.arange(0,steps+1,1)
-    dsteps=np.zeros(steps+1)
-    
-    for i in range(0, np.size(dsteps)-1):	                
-        good=np.logical_and(chist>hsteps[i],chist<hsteps[i+1]).nonzero()
-        dsteps[i]=np.min(bin_centre[good])
-    
-    dsteps[np.size(dsteps)-1]=np.max(dens)
-    
-    return dsteps
+   chist=np.cumsum(hist)
+   pitch=np.max(chist)/float(steps)
+   hsteps=pitch*np.arange(0,steps+1,1)
+   dsteps=np.zeros(steps+1)
 
-def roparameter(cosphi, hist, s_cosphi=0.25):
-    """
-    ...
-    
-        Parameters
-        ----------
-        cosphi : ...
-             
-        hist   : ...
-             
-       
-        s_cosphi : ...
-             
-        
-        Returns
-        -------
-        ...
-    
-    """
-    	
-    perp=(np.abs(cosphi)>1.-s_cosphi).nonzero()
-    para=(np.abs(cosphi)<s_cosphi).nonzero()
-    
-    xi=(np.sum(hist[para])-np.sum(hist[perp]))/float(np.sum(hist[para])+np.sum(hist[perp]))
-    
-    return xi
+   for i in range(0, np.size(dsteps)-1):	                
+      good=np.logical_and(chist>hsteps[i],chist<hsteps[i+1]).nonzero()
+      dsteps[i]=np.min(bin_centre[good])
 
-def hro3D(dens, Bx, By, Bz, steps=10, hsize=21, mind=0, outh=[0,4,9], pxksz=3, label=r'$n$'):
-    """
-    Calculate the histogram of relative orientations (HRO) in three-dimensional data.
+   dsteps[np.size(dsteps)-1]=np.max(dens)
 
-        Parameters
-        ----------
-        dens   : ...
+   return dsteps
 
-        Bx,y,z : ...
 
-        steps  : ...
+# ============================================================================================================
+def roparameterhist(cosphi, hsize=15, ppwin=0.25, w=None):
 
-        hsize  : ...
+   # Calculate the relative orientation parameter $\xi$, Eq. 13 in Soler et al. 2013
+   #
+   # INPUTS
+   # cosphi     - relative orientation angles in 3D, its range should be [-1.0,1.0]
+   # hsize      - size of the histogram used for the calculation of the relative orientation parameter
+   # ppwin      - histogram range for the definitions of parallel (- ppwin < cos(phi) < ppwin) or  
+   #              perpendicular (-1 < cos(phi) < -1.+ppwin && 1-ppwin < cos(phi) < 1.)
+   # OUTPUTS
+   # xi          - relative orientation parameter  
 
-        mind   : ...
-
-        outh   : ...
-
-        Returns
-        -------
-        hro   : ...
-
-        cdens : ...
-
-        zeta  : ...
-
-    """
-
-    cosphi=roangles3D(dens, Bx, By, Bz, pxksz=pxksz)
-    dsteps=equibins(dens, steps=steps, mind=mind)
-
-    hros   =np.zeros([steps,hsize])
-    cdens  =np.zeros(steps)
-    xi     =np.zeros(steps)
-    meancos=np.zeros(steps)
-    scube = 0.*dens
-
-    for i in range(0, np.size(dsteps)-1):
-        good=np.logical_and(dens>dsteps[i],dens<dsteps[i+1]).nonzero()
-        hist, bin_edges=np.histogram(cosphi[good], bins=hsize, range=(-1.,1.))	
-        bin_centre=0.5*(bin_edges[0:np.size(bin_edges)-1]+bin_edges[1:np.size(bin_edges)])
-        hros[i,:]=hist
-        scube[good]=i
-        cdens[i]=np.mean([dsteps[i],dsteps[i+1]])
-        xi[i]=roparameter(bin_centre, hist)
-        meancos[i]=np.mean(cosphi[good]) 
-
-    outsteps = np.size(outh)
-    color    = iter(cm.cool(np.linspace(0, 1, outsteps)))
-
-    fig = plt.figure(figsize=(6.0,6.0))
-    plt.rc('font', size=10)
-    ax1=plt.subplot(111)
-    for i in range(0, outsteps):
-        c = next(color)
-        labeltext = str(np.round(dsteps[outh[i]],2))+' < '+label+' < '+str(np.round(dsteps[outh[i]+1],2)) 
-        ax1.plot(bin_centre, hros[outh[i],:], '-', linewidth=2, c=c, label=labeltext) #drawstyle
-    ax1.set_xlabel(r'cos($\phi$)')
-    ax1.set_ylabel('Counts')
-    ax1.tick_params(axis='y', labelrotation=90) 
-    ax1.legend()
-    plt.tight_layout()
-    plt.show()
-
-    fig = plt.figure(figsize=(8.0,4.0))
-    plt.rc('font', size=10)
-    ax1=plt.subplot(111)
-    ax1.semilogx(cdens, xi, 'o-', linewidth=2, color='blue')
-    ax1.tick_params(axis='y', labelrotation=90) 
-    ax1.axhline(y=0., c='k', ls='--')	
-    ax1.set_xlabel(r'log$_{10}$ ($n_{\rm H}/$cm$^{-3}$)')
-    ax1.set_ylabel(r'$\xi$')
-    plt.tight_layout()
-    #plt.savefig(prefix + '-' + 'ROvsLogNH' + 'Thres' + "%d" % (thr) + '.png')
-    plt.show()
-
-    fig = plt.figure(figsize=(8.0,4.0))
-    plt.rc('font', size=10)
-    ax1=plt.subplot(111)
-    ax1.semilogx(cdens, meancos, 'o-', linewidth=2, color='blue')
-    ax1.tick_params(axis='y', labelrotation=90)
-    ax1.axhline(y=0., c='k', ls='--')  
-    ax1.set_xlabel(r'log$_{10}$ ($n_{\rm H}/$cm$^{-3}$)')
-    ax1.set_ylabel(r'$\left<\cos\theta\right>$')
-    plt.tight_layout()
-    plt.show()
+   if (w is None):
+      w=np.ones_like(cosphi)
+   else:
+      assert np.shape(cosphi) == np.shape(w), "Dimensions of cosphi and w must match"  
   
-    return hros, cdens, xi
+   hist, bin_edges = np.histogram(cosphi, bins=hsize, range=(-1.,1.), weights=w)
+   bin_centres=0.5*(bin_edges[0:np.size(bin_edges)-1]+bin_edges[1:np.size(bin_edges)])  
+ 
+   xi, s_xi = roparameter(bin_centres, hist, ppwin=ppwin)
+ 	
+   return xi, s_xi
+
+# ===================================================================================================
+def roparameter(cosphi, hist, ppwin=0.25):
+    # Calculate the relative orientation parameter $\xi$ as defined in Planck intermediate results. XXXV. A&A 586A (2016) 138P.
+    #
+    # INPUTS
+    # cosphi      - histogram bin centres 
+    # hist        - histogram counts
+    # ppwin       - range for the definitions of parallel (0 < phi < s_phi and 180-s_phi < phi < 180) or 
+    #               perpendicular (90-s_phi < phi < 90+s_phi)
+    # OUTPUTS
+    # xi          - relative orientation parameter
+  
+    perp=(np.abs(cosphi)>1.-ppwin).nonzero()
+    para=(np.abs(cosphi)<ppwin).nonzero()
+  
+    temp=float(np.sum(hist[para])+np.sum(hist[perp]))
+    if (temp > 0.):
+       xi=float(np.sum(hist[para])-np.sum(hist[perp]))/float(np.sum(hist[para])+np.sum(hist[perp]))
+       s_xi=2.*np.sqrt((np.sum(hist[para])*np.std(hist[perp]))**2+(np.sum(hist[perp])*np.std(hist[para]))**2)/(np.sum(hist[para])+np.sum(hist[perp]))**2
+    else:
+       xi=np.nan
+       s_xi=np.nan
+
+    return xi, s_xi
+
+# ============================================================================================================
+def hro3D(dens, Bx, By, Bz, steps=10, hsize=21, mind=None, outh=[0,4,9], pxksz=3, label=r'$n$', weights=None):
+
+   # Calculate the relative orientation parameter $\xi$, Eq. 13 in Soler et al. 2013
+   #
+   # INPUTS
+   # cosphi     - relative orientation angles in 3D, its range should be [-1.0,1.0]
+   # hsize      - size of the histogram used for the calculation of the relative orientation parameter
+   # ppwin      - histogram range for the definitions of parallel (- ppwin < cos(phi) < ppwin) or  
+   #              perpendicular (-1 < cos(phi) < -1.+ppwin && 1-ppwin < cos(phi) < 1.)
+   # OUTPUTS
+   # xi          - relative orientation parameter 
+ 
+   if (weights is None):
+      # Account for the correlation introduced by the kernel size
+      weights=(1./pxksz**2)*np.ones_like(dens)
+   else:
+      assert np.shape(dens) == np.shape(weights), "Dimensions of cosphi and w must match"
+
+   cosphi=roangles3D(dens, Bx, By, Bz, pxksz=pxksz)
+   dsteps=equibins(dens, steps=steps, mind=mind)
+
+   hros   =np.zeros([steps,hsize])
+   cdens  =np.zeros(steps)
+   xi     =np.zeros(steps)
+   s_xi   =np.zeros(steps)
+   meancos=np.zeros(steps)
+   scube = 0.*dens
+
+   for i in tqdm(range(0, np.size(dsteps)-1)):
+      good=np.logical_and(dens>dsteps[i],dens<dsteps[i+1]).nonzero()
+      hist, bin_edges=np.histogram(cosphi[good], bins=hsize, range=(-1.,1.), weights=weights[good])	
+      bin_centre=0.5*(bin_edges[0:np.size(bin_edges)-1]+bin_edges[1:np.size(bin_edges)])
+      hros[i,:]=hist
+      scube[good]=i
+      cdens[i]=np.mean([dsteps[i],dsteps[i+1]])
+      xi[i], s_xi[i]=roparameter(bin_centre, hist)
+      meancos[i]=np.mean(weights[good]*cosphi[good]) 
+
+   outsteps = np.size(outh)
+   color    = iter(cm.cool(np.linspace(0, 1, outsteps)))
+
+   fig = plt.figure(figsize=(6.0,6.0))
+   plt.rc('font', size=10)
+   ax1=plt.subplot(111)
+   for i in range(0, outsteps):
+      c = next(color)
+      labeltext = str(np.round(dsteps[outh[i]],2))+' < '+label+' < '+str(np.round(dsteps[outh[i]+1],2)) 
+      ax1.plot(bin_centre, hros[outh[i],:], '-', linewidth=2, c=c, label=labeltext) #drawstyle
+   ax1.set_xlabel(r'cos($\phi$)')
+   ax1.set_ylabel('Counts')
+   ax1.tick_params(axis='y', labelrotation=90) 
+   ax1.legend()
+   plt.tight_layout()
+   plt.show()
+
+   fig = plt.figure(figsize=(8.0,4.0))
+   plt.rc('font', size=10)
+   ax1=plt.subplot(111)
+   ax1.semilogx(cdens, xi, 'o-', linewidth=2, color='blue')
+   ax1.tick_params(axis='y', labelrotation=90) 
+   ax1.axhline(y=0., c='k', ls='--')	
+   ax1.set_xlabel(r'log$_{10}$ ($n_{\rm H}/$cm$^{-3}$)')
+   ax1.set_ylabel(r'$\xi$')
+   plt.tight_layout()
+   #plt.savefig(prefix + '-' + 'ROvsLogNH' + 'Thres' + "%d" % (thr) + '.png')
+   plt.show()
+
+   fig = plt.figure(figsize=(8.0,4.0))
+   plt.rc('font', size=10)
+   ax1=plt.subplot(111)
+   ax1.semilogx(cdens, meancos, 'o-', linewidth=2, color='blue')
+   ax1.tick_params(axis='y', labelrotation=90)
+   ax1.axhline(y=0., c='k', ls='--')  
+   ax1.set_xlabel(r'log$_{10}$ ($n_{\rm H}/$cm$^{-3}$)')
+   ax1.set_ylabel(r'$\left<\cos\theta\right>$')
+   plt.tight_layout()
+   plt.show()
+  
+   #return hros, cdens, xi
+   return {'hros': hros, 'abins': cdens, 'xi': xi, 's_xi': s_xi, 'meancos': meancos}
 
 
 # Testing the hro3D. should go in the test script or something.
